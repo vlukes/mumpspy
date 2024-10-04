3fbdbc0dbac3a8ae7b08371138d8d1fdbda5a219
Vladimir Lukes <vlukes@kme.zcu.cz>
2024-10-04 12:21:10 +0200

    add new test, rearrange code

--- src/mumpspy_test.py
+++ src/mumpspy_test.py
@@ -6,10 +6,13 @@ Expected results:
     complex system: [1 + 0i, 1 + 1i, 0 + 2i, 2 + 2i]
 """
 
-import numpy as np
+import numpy as nm
 import mumpspy
 import scipy.sparse as sps
 import scipy.linalg as sla
+
+
+OUTPUT_LEVEL = 1  # 1=brief or 2=verbose
 
 
 def print_arr(a):
@@ -17,93 +20,106 @@ def print_arr(a):
 
 
 def check_solution(exp, sol):
-    return 'passed' if np.linalg.norm(exp_x - x) < 1e-9 else 'failed!'
+    return 'passed' if nm.linalg.norm(exp_x - x) < 1e-9 else 'failed!'
 
 
-# real-valued system
-print('real-valued system')
-print('==================')
-ridx = np.array([1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4], dtype='i')
-cidx = np.array([1, 2, 4, 1, 2, 3, 3, 4, 1, 2, 3, 4], dtype='i')
-val = np.array([2, 3, -1, 1, 2, -3, 2, -1, 2, -5, 2, 1], dtype='d')
-b = np.array([4, -4, 2, 2], dtype='d')
-exp_x = np.array([1, 2, 3, 4], dtype='d')
+def print_system(title, A, b, x, exp_x):
+    check = check_solution(exp_x, x)
 
+    title = f'{title}: {check}'
+    print(title)
+
+    if OUTPUT_LEVEL > 1:
+        print('=' * len(title))
+        s = '\n       '.join([print_arr(ii) for ii in A.toarray()])
+        print(f'  A: [ {s} ]')
+        print(f'  b: [ {print_arr(b)} ]')
+        print(f'  expected result  : [ {print_arr(exp_x)} ]')
+        print(f'  calculated result: [ {print_arr(nm.round(x, 9))} ]')
+        print()
+
+
+exp_x = nm.array([1, 2, 3, 4], dtype='d')
+schur_list = nm.array([3, 4])
+
+title = 'real-valued symmetric system'
+ridx = nm.array([1, 1, 1, 2, 2, 3, 3, 4, 4, 4])
+cidx = nm.array([1, 3, 4, 2, 4, 1, 3, 1, 2, 4])
+val = nm.array([1, 2, -1, 3, -2, 2, -2, -1, -2, 1])
+b = nm.array([3, -2, -4, -1])
 A = sps.coo_matrix((val, (ridx - 1, cidx - 1)), shape=(4, 4))
-
-s = '\n       '.join([print_arr(ii) for ii in A.toarray()])
-print(f'  A: [ {s} ]')
-print(f'  b: [ {print_arr(b)} ]')
-print(f'  expected result:        [ {print_arr(exp_x)} ]')
 
 solver = mumpspy.MumpsSolver()
 solver.set_rcd_mtx(ridx, cidx, val, 4)
+solver.set_rhs(b)  # b will be overwritten!
+x = solver.solve()  # x is b: True
+del solver
+
+print_system(title + ' (set_rcd_mtx)', A, b, x, exp_x)
+
+solver = mumpspy.MumpsSolver(is_sym=True)
+solver.set_mtx(A)
 x = solver.solve(b)
+del solver
 
-check = check_solution(exp_x, x)
-print(f'  solution (set_rcd_mtx): [ {print_arr(np.round(x, 9))} ] ({check})')
+print_system(title + ' (set_mtx)', A, b, x, exp_x)
 
+solver = mumpspy.MumpsSolver(is_sym=True)
+solver.set_mtx(A)
+bb = nm.vstack([2*b, b]).T
+exp_xx = nm.vstack([2*exp_x, exp_x]).T
+xx = solver.solve(bb)
+del solver
+
+print_system(title + ' (multiple RHS)', A, bb, xx, exp_xx)
+
+solver = mumpspy.MumpsSolver(is_sym=True)
+solver.set_mtx(A, factorize=False)
+x = solver.schur_solve(schur_list, b)
+del solver
+
+print_system(title + ' (Schur complement)', A, b, x, exp_x)
+
+title = 'real-valued non-symmetric system'
+ridx = nm.array([1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4])
+cidx = nm.array([1, 2, 4, 1, 2, 3, 3, 4, 1, 2, 3, 4])
+val = nm.array([2, 3, -1, 1, 2, -3, 2, -1, 2, -5, 2, 1])
+b = nm.array([4, -4, 2, 2])
+A = sps.coo_matrix((val, (ridx - 1, cidx - 1)), shape=(4, 4))
+
+solver = mumpspy.MumpsSolver()
 solver.set_mtx(A)
 solver.set_rhs(b)  # b will be overwritten!
 x = solver.solve()  # x is b: True
-
-check = check_solution(exp_x, x)
-print(f'  solution (set_mtx):     [ {print_arr(np.round(x, 9))} ] ({check})\n')
-
 del solver
 
+print_system(title, A, b, x, exp_x)
 
-# complex-valued system
-print('complex-valued system')
-print('=====================')
-ridx = np.array([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4], dtype='i')
-cidx = np.array([1, 2, 3, 1, 2, 3, 2, 3, 4, 3, 4], dtype='i')
-val = np.array([1 + 2j, -2j, 3 + 3j, 1j, 7, 3j, -1, 1j, 1, 3 + 5j, 2 - 3j],
-               dtype='D')
-b = np.array([-3 + 6j, 1 + 8j, -1 + 1j, 4j], dtype='D')
-exp_x = np.array([1 + 0j, 1 + 1j, 0 + 2j, 2 + 2j], dtype='D')
+solver = mumpspy.MumpsSolver()
+solver.set_mtx(A, factorize=False)
+x = solver.schur_solve(schur_list, b)
+del solver
 
+print_system(title + ' (Schur complement)', A, b, x, exp_x)
+
+title = 'complex-valued non-symmetrix system'
+ridx = nm.array([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4])
+cidx = nm.array([1, 2, 3, 1, 2, 3, 2, 3, 4, 3, 4])
+val = nm.array([1 + 2j, -2j, 3 + 3j, 1j, 7, 3j, -1, 1j, 1, 3 + 5j, 2 - 3j])
+b = nm.array([-3 + 6j, 1 + 8j, -1 + 1j, 4j])
+exp_x = nm.array([1 + 0j, 1 + 1j, 0 + 2j, 2 + 2j])
 A = sps.coo_matrix((val, (ridx - 1, cidx - 1)), shape=(4, 4))
-
-s = '\n       '.join([print_arr(ii) for ii in A.toarray()])
-print(f'  A: [ {s} ]')
-print(f'  b: [ {print_arr(b)} ]')
-print(f'  expected result: [ {print_arr(exp_x)} ]')
 
 solver = mumpspy.MumpsSolver(system='complex')
 solver.set_mtx(A)
 x = solver.solve(b)
-
-check = check_solution(exp_x, x)
-print(f'  solution:        [ {print_arr(np.round(x, 9))} ] ({check})\n')
-
 del solver
 
+print_system(title, A, b, x, exp_x)
 
-# Schur complement
-print('Schur complement')
-print('================')
-ridx = np.array([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4], dtype='i')
-cidx = np.array([1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 1, 2], dtype='i')
-val = np.array([1, 2, 2, 1, 1, 3, -1, 2, 1, 1, 3, 1], dtype='d')
-b = np.array([15, 12, 3, 5], dtype='d')
-schur_list = np.array([3, 4], dtype='i')
-exp_x = np.array([1, 2, 3, 4], dtype='d')
-
-A = sps.coo_matrix((val, (ridx - 1, cidx - 1)), shape=(4, 4))
-
-s = '\n       '.join([print_arr(ii) for ii in A.toarray()])
-print('  A: [ %s ]' % s)
-print('  b: [ %s ]' % print_arr(b))
-print('  Schur list: [ %s ]' % print_arr(schur_list))
-print('  expected result: [ %s ]' % print_arr(exp_x))
-
-solver = mumpspy.MumpsSolver()
-solver.set_mtx(A)
+solver = mumpspy.MumpsSolver(system='complex')
+solver.set_mtx(A, factorize=False)
 x = solver.schur_solve(schur_list, b)
-
-check = check_solution(exp_x, x)
-print(f'  solution:        [ {print_arr(np.round(x, 9))} ] ({check})\n')
-
 del solver
 
+print_system(title + ' (Schur complement)', A, b, x, exp_x)
