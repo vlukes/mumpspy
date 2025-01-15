7bd4669dfbda489d1a9441e7a65dc51b3dfcae2f
Luc LAURENT <luc.laurent@cnam.fr>
2025-01-15 13:50:33 +0100

    first new version with all tests working with MUMPS 5.6.1 with complex/real (single/double precision), switch to version 1.4.0b

--- mumpspy/mumps_lib_c_struc.py
+++ mumpspy/mumps_lib_c_struc.py
@@ -1,7 +1,7 @@
 import ctypes
 
-MIN_SUPPORTED_VERSION = '4.10.0'
-MAX_SUPPORTED_VERSION = '5.7.999'
+MIN_SUPPORTED_VERSION = "4.10.0"
+MAX_SUPPORTED_VERSION = "5.7.999"
 
 AUX_LENGTH = 16 * 1024
 
@@ -21,7 +21,7 @@ MumpsComplex16 = MumpsReal8
 PMumpsComplex16 = c_pointer(MumpsComplex16)
 
 
-def get_all_fields(precision='double'):
+def get_all_fields(precision="double"):
     """Get all declared and updated fields for MUMPS C structure for a given precision.
 
     Args:
@@ -30,205 +30,230 @@ def get_all_fields(precision='double'):
     Returns:
         mumps_c_fields: basics fields for MUMPS C structure
         mumps_c_updates: incremental updates for MUMPS C structure
-    """ 
+    """
 
     cMumpsInt = MumpsInt
     cPMumpsInt = PMumpsInt
     cMumpsInt8 = MumpsInt8
     cPMumpsInt8 = PMumpsInt8
-    if precision == 'single':
+    if precision == "single":
         cMumpsReal = MumpsReal
         cPMumpsReal = PMumpsReal
         cMumpsComplex = MumpsComplex
         cPMumpsComplex = PMumpsComplex
-    elif precision == 'double':
+    elif precision == "double":
         cMumpsReal = MumpsReal8
         cPMumpsReal = PMumpsReal8
         cMumpsComplex = MumpsComplex16
         cPMumpsComplex = PMumpsComplex16
     else:
-        raise ValueError(f'Precision {precision} not supported!')
+        raise ValueError(f"Precision {precision} not supported!")
     #
 
     mumps_c_fields = [  # MUMPS 4.10.0
-        ('sym', cMumpsInt),
-        ('par', cMumpsInt),
-        ('job', cMumpsInt),
-        ('comm_fortran', cMumpsInt),
-        ('icntl', cMumpsInt * 40),
-        ('cntl', cMumpsReal * 15),
-        ('n', cMumpsInt),
+        ("sym", cMumpsInt),
+        ("par", cMumpsInt),
+        ("job", cMumpsInt),
+        ("comm_fortran", cMumpsInt),
+        ("icntl", cMumpsInt * 40),
+        ("cntl", cMumpsReal * 15),
+        ("n", cMumpsInt),
         #
-        ('nz_alloc', cMumpsInt),
+        ("nz_alloc", cMumpsInt),
         # /* Assembled entry */
-        ('nz', cMumpsInt),
-        ('irn', cPMumpsInt),
-        ('jcn', cPMumpsInt),
-        ('a', cPMumpsComplex),
+        ("nz", cMumpsInt),
+        ("irn", cPMumpsInt),
+        ("jcn", cPMumpsInt),
+        ("a", cPMumpsComplex),
         # /* Distributed entry */
-        ('nz_loc', cMumpsInt),
-        ('irn_loc', cPMumpsInt),
-        ('jcn_loc', cPMumpsInt),
-        ('a_loc', cPMumpsComplex),
+        ("nz_loc", cMumpsInt),
+        ("irn_loc", cPMumpsInt),
+        ("jcn_loc", cPMumpsInt),
+        ("a_loc", cPMumpsComplex),
         # /* Element entry */
-        ('nelt', cMumpsInt),
-        ('eltptr', cPMumpsInt),
-        ('eltvar', cPMumpsInt),
-        ('a_elt', cPMumpsComplex),
+        ("nelt", cMumpsInt),
+        ("eltptr", cPMumpsInt),
+        ("eltvar", cPMumpsInt),
+        ("a_elt", cPMumpsComplex),
         # /* Ordering, if given by user */
-        ('perm_in', cPMumpsInt),
+        ("perm_in", cPMumpsInt),
         # /* Orderings returned to user */
-        ('sym_perm', cPMumpsInt),
-        ('uns_perm', cPMumpsInt),
+        ("sym_perm", cPMumpsInt),
+        ("uns_perm", cPMumpsInt),
         # /* Scaling (input only in this version) */
-        ('colsca', cPMumpsReal),
-        ('rowsca', cPMumpsReal),
+        ("colsca", cPMumpsReal),
+        ("rowsca", cPMumpsReal),
         # /* RHS, solution, ouptput data and statistics */
-        ('rhs', cPMumpsComplex),
-        ('redrhs', cPMumpsComplex),
-        ('rhs_sparse', cPMumpsComplex),
-        ('sol_loc', cPMumpsComplex),
-        ('irhs_sparse', cPMumpsInt),
-        ('irhs_ptr', cPMumpsInt),
-        ('isol_loc', cPMumpsInt),
-        ('nrhs', cPMumpsInt),
-        ('lrhs', cPMumpsInt),
-        ('lredrhs', cPMumpsInt),
-        ('nz_rhs', cPMumpsInt),
-        ('lsol_loc', cPMumpsInt),
-        ('schur_mloc', cPMumpsInt),
-        ('schur_nloc', cPMumpsInt),
-        ('schur_lld', cPMumpsInt),
-        ('mblock', cPMumpsInt),
-        ('nblock', cPMumpsInt),
-        ('nprow', cPMumpsInt),
-        ('npcol', cPMumpsInt),
-        ('info', cPMumpsInt * 40),
-        ('infog', cPMumpsInt * 40),
-        ('rinfo', cMumpsReal * 40),
-        ('rinfog', cMumpsReal * 40),
+        ("rhs", cPMumpsComplex),
+        ("redrhs", cPMumpsComplex),
+        ("rhs_sparse", cPMumpsComplex),
+        ("sol_loc", cPMumpsComplex),
+        ("irhs_sparse", cPMumpsInt),
+        ("irhs_ptr", cPMumpsInt),
+        ("isol_loc", cPMumpsInt),
+        ("nrhs", cMumpsInt),
+        ("lrhs", cMumpsInt),
+        ("lredrhs", cMumpsInt),
+        ("nz_rhs", cMumpsInt),
+        ("lsol_loc", cMumpsInt),
+        ("schur_mloc", cMumpsInt),
+        ("schur_nloc", cMumpsInt),
+        ("schur_lld", cMumpsInt),
+        ("mblock", cMumpsInt),
+        ("nblock", cMumpsInt),
+        ("nprow", cMumpsInt),
+        ("npcol", cMumpsInt),
+        ("info", cMumpsInt * 40),
+        ("infog", cMumpsInt * 40),
+        ("rinfo", cMumpsReal * 40),
+        ("rinfog", cMumpsReal * 40),
         # /* Null space */
-        ('deficiency', cPMumpsInt),
-        ('pivnul_list', cPMumpsInt),
-        ('mapping', cPMumpsInt),
+        ("deficiency", cMumpsInt),
+        ("pivnul_list", cPMumpsInt),
+        ("mapping", cPMumpsInt),
         # /* Schur */
-        ('size_schur', cMumpsInt),
-        ('listvar_schur', cPMumpsInt),
-        ('schur', cPMumpsComplex),
+        ("size_schur", cMumpsInt),
+        ("listvar_schur", cPMumpsInt),
+        ("schur", cPMumpsComplex),
         # /* Internal parameters */
-        ('instance_number', cMumpsInt),
-        ('wk_user', cPMumpsComplex),
+        ("instance_number", cMumpsInt),
+        ("wk_user", cPMumpsComplex),
         # /* Version number:
         #  length in FORTRAN + 1 for final \0 + 1 for alignment */
-        ('version_number', ctypes.c_char * 16),
+        ("version_number", ctypes.c_char * 16),
         # /* For out-of-core */
-        ('ooc_tmpdir', ctypes.c_char * 256),
-        ('ooc_prefix', ctypes.c_char * 64),
+        ("ooc_tmpdir", ctypes.c_char * 256),
+        ("ooc_prefix", ctypes.c_char * 64),
         # /* To save the matrix in matrix market format */
-        ('write_problem', ctypes.c_char * 256),
-        ('lwk_user', cMumpsInt),
+        ("write_problem", ctypes.c_char * 256),
+        ("lwk_user", cMumpsInt),
     ]
 
-
     mumps_c_updates = {  # incremental updates related to version 4.10.0
-        '5.0.0': [
-            ('new_after', 'icntl', ('keep', cMumpsInt * 500)),
-            ('new_after', 'cntl', [
-                ('dkeep', cMumpsReal * 130),
-                ('keep8', cMumpsInt8 * 150),
-            ]),
-            ('new_after', 'rowsca', [
-                ('colsca_from_mumps', cMumpsInt),
-                ('rowsca_from_mumps', cMumpsInt),
-            ]),
-            ('replace', 'version_number', ctypes.c_char * 27),
+        "5.0.0": [
+            ("new_after", "icntl", ("keep", cMumpsInt * 500)),
+            (
+                "new_after",
+                "cntl",
+                [
+                    ("dkeep", cMumpsReal * 130),
+                    ("keep8", cMumpsInt8 * 150),
+                ],
+            ),
+            (
+                "new_after",
+                "rowsca",
+                [
+                    ("colsca_from_mumps", cMumpsInt),
+                    ("rowsca_from_mumps", cMumpsInt),
+                ],
+            ),
+            ("replace", "version_number", ctypes.c_char * 27),
         ],
-        '5.1.0': [
-            ('replace', 'dkeep', cMumpsReal * 230),
-            ('new_after', 'nz', ('nnz', cMumpsInt8)),
-            ('new_after', 'nz_loc', ('nnz_loc', cMumpsInt8)),
-            ('replace', 'version_number', ctypes.c_char * 32),
-            ('new_after', 'lwk_user', [
-                # /* For save/restore feature */
-                ('save_dir', ctypes.c_char * 256),
-                ('save_prefix', ctypes.c_char * 256),
-            ]),
+        "5.1.0": [
+            ("replace", "dkeep", cMumpsReal * 230),
+            ("new_after", "nz", ("nnz", cMumpsInt8)),
+            ("new_after", "nz_loc", ("nnz_loc", cMumpsInt8)),
+            ("replace", "version_number", ctypes.c_char * 32),
+            (
+                "new_after",
+                "lwk_user",
+                [
+                    # /* For save/restore feature */
+                    ("save_dir", ctypes.c_char * 256),
+                    ("save_prefix", ctypes.c_char * 256),
+                ],
+            ),
         ],
-        '5.2.0': [
-            ('replace', 'icntl', cMumpsInt * 60),
-            ('new_after', 'sol_loc', ('rhs_loc', cPMumpsComplex)),
-            ('new_after', 'isol_loc', ('irhs_loc', cPMumpsInt)),
-            ('new_after', 'lsol_loc', [
-                ('nloc_rhs', cMumpsInt),
-                ('lrhs_loc', cMumpsInt),
-            ]),
-            ('replace', 'info', cMumpsInt * 80),
-            ('replace', 'infog', cMumpsInt * 80),
-            ('new_after', 'save_prefix', ('metis_options', cMumpsInt * 40)),
+        "5.2.0": [
+            ("replace", "icntl", cMumpsInt * 60),
+            ("new_after", "sol_loc", ("rhs_loc", cPMumpsComplex)),
+            ("new_after", "isol_loc", ("irhs_loc", cPMumpsInt)),
+            (
+                "new_after",
+                "lsol_loc",
+                [
+                    ("nloc_rhs", cMumpsInt),
+                    ("lrhs_loc", cMumpsInt),
+                ],
+            ),
+            ("replace", "info", cMumpsInt * 80),
+            ("replace", "infog", cMumpsInt * 80),
+            ("new_after", "save_prefix", ("metis_options", cMumpsInt * 40)),
         ],
-        '5.3.0': [
-            ('new_after', 'n', ('nblk', cMumpsInt)),
-            ('new_after', 'a_elt', [
-                # /* Matrix by blocks */
-                ('blkptr', cPMumpsInt),
-                ('blkvar', cPMumpsInt),
-            ]),
+        "5.3.0": [
+            ("new_after", "n", ("nblk", cMumpsInt)),
+            (
+                "new_after",
+                "a_elt",
+                [
+                    # /* Matrix by blocks */
+                    ("blkptr", cPMumpsInt),
+                    ("blkvar", cPMumpsInt),
+                ],
+            ),
         ],
-        '5.7.0': [
-            ('new_after', 'npcol', ('ld_rhsintr', cMumpsInt)),
-            ('new_after', 'mapping', ('singular_values', cPMumpsReal)),
-            ('delete', 'instance_number'),
-            ('replace', 'ooc_tmpdir', ctypes.c_char * 1024),
-            ('replace', 'ooc_prefix', ctypes.c_char * 256),
-            ('replace', 'write_problem', ctypes.c_char * 1024),
-            ('replace', 'save_dir', ctypes.c_char * 1024),
-            ('new_after', 'metis_options', ('instance_number', cMumpsInt)),
+        "5.7.0": [
+            ("new_after", "npcol", ("ld_rhsintr", cMumpsInt)),
+            ("new_after", "mapping", ("singular_values", cPMumpsReal)),
+            ("delete", "instance_number"),
+            ("replace", "ooc_tmpdir", ctypes.c_char * 1024),
+            ("replace", "ooc_prefix", ctypes.c_char * 256),
+            ("replace", "write_problem", ctypes.c_char * 1024),
+            ("replace", "save_dir", ctypes.c_char * 1024),
+            ("new_after", "metis_options", ("instance_number", cMumpsInt)),
         ],
     }
+
     return mumps_c_fields, mumps_c_updates
 
 
 def version_to_int(v):
     """Convert the version string to an integer ('5.2.1' --> 5002001)."""
-    return sum(int(vk) * 10**(3*k) for k, vk in enumerate(v.split('.')[::-1]))
+    return sum(int(vk) * 10 ** (3 * k) for k, vk in enumerate(v.split(".")[::-1]))
 
 
-def get_mumps_c_fields(version=None, precision='double'):
+def get_mumps_c_fields(version=None, precision="double"):
     """Return the MUMPS C fields for a given MUMPS version."""
+
     def update_fields(f, update_f):
         for uf in update_f:
             fk = [k for k, _ in f]
             idx = fk.index(uf[1])
 
-            if uf[0] == 'replace':
+            if uf[0] == "replace":
                 f[idx] = (uf[1], uf[2])
-            elif uf[0] == 'delete':
+            elif uf[0] == "delete":
                 del f[idx]
-            elif uf[0] == 'new_after':
+            elif uf[0] == "new_after":
                 if isinstance(uf[2], list):
-                    f[(idx + 1):(idx + 1)] = uf[2]
+                    f[(idx + 1) : (idx + 1)] = uf[2]
                 else:
                     f.insert(idx + 1, uf[2])
 
         return f
-    
-    mumps_c_fields,mumps_c_updates = get_all_fields(precision=precision)
+
+    mumps_c_fields, mumps_c_updates = get_all_fields(precision=precision)
 
     if version is None:
-        fields = mumps_c_fields[:5] + [('aux', ctypes.c_uint8 * AUX_LENGTH)]
+        fields = mumps_c_fields[:5] + [("aux", ctypes.c_uint8 * AUX_LENGTH)]
     else:
         update_keys = list(mumps_c_updates.keys())
         update_keys.sort()
 
         vnum = version_to_int(version)
         if vnum < version_to_int(MIN_SUPPORTED_VERSION):
-            msg = (f'MUMPS version {version} not supported! '
-                f'({version} < {MIN_SUPPORTED_VERSION})')
+            msg = (
+                f"MUMPS version {version} not supported! "
+                f"({version} < {MIN_SUPPORTED_VERSION})"
+            )
             raise ValueError(msg)
 
         if vnum > version_to_int(MAX_SUPPORTED_VERSION):
-            msg = (f'MUMPS version {version} not supported! '
-                f'({version} > {MAX_SUPPORTED_VERSION})')
+            msg = (
+                f"MUMPS version {version} not supported! "
+                f"({version} > {MAX_SUPPORTED_VERSION})"
+            )
             raise ValueError(msg)
 
         fields = mumps_c_fields.copy()
@@ -241,8 +266,9 @@ def get_mumps_c_fields(version=None, precision='double'):
     return fields
 
 
-def define_mumps_c_struc(version=None, precision='double'):
+def define_mumps_c_struc(version=None, precision="double"):
     """Return MUMPS_C_STRUC class with given fields."""
+
     class Mumps_c_struc(ctypes.Structure):
         _fields_ = get_mumps_c_fields(version, precision=precision)
 
--- mumpspy/test/mumpspy_test.py
+++ mumpspy/test/mumpspy_test.py
@@ -17,8 +17,8 @@ OUTPUT_LEVEL = 1  # 1=brief or 2=verbose
 _RTOL_double = 1e-9
 _RTOL_single = 1e-6
 
-optionsreal = ['float32', 'float64']
-optionscomplex = ['complex64', 'complex128']
+optionsreal = ["float32", "float64"]
+optionscomplex = ["complex64", "complex128"]
 
 
 def print_arr(a):
@@ -26,188 +26,210 @@ def print_arr(a):
 
 
 def check_solution(exp, sol):
-    return 'passed' if nm.linalg.norm(exp_x - x) < 1e-9 else 'failed!'
+    return "passed" if nm.linalg.norm(exp_x - x) < 1e-9 else "failed!"
 
 
 def print_system(title, A, b, x, exp_x):
     check = check_solution(exp_x, x)
 
-    title = f'{title}: {check}'
+    title = f"{title}: {check}"
     print(title)
 
     if OUTPUT_LEVEL > 1:
-        print('=' * len(title))
-        s = '\n       '.join([print_arr(ii) for ii in A.toarray()])
-        print(f'  A: [ {s} ]')
-        print(f'  b: [ {print_arr(b)} ]')
-        print(f'  expected result  : [ {print_arr(exp_x)} ]')
-        print(f'  calculated result: [ {print_arr(nm.round(x, 9))} ]')
+        print("=" * len(title))
+        s = "\n       ".join([print_arr(ii) for ii in A.toarray()])
+        print(f"  A: [ {s} ]")
+        print(f"  b: [ {print_arr(b)} ]")
+        print(f"  expected result  : [ {print_arr(exp_x)} ]")
+        print(f"  calculated result: [ {print_arr(nm.round(x, 9))} ]")
         print()
 
 
-def loadSystem(name='real-sym'):
-    if name == 'real-sym':
-        title = 'real-valued symmetric system'
+def loadSystem(name="real-sym"):
+    if name == "real-sym":
+        title = "real-valued symmetric system"
         ridx = nm.array([1, 1, 1, 2, 2, 3, 3, 4, 4, 4])
         cidx = nm.array([1, 3, 4, 2, 4, 1, 3, 1, 2, 4])
         val = nm.array([1, 2, -1, 3, -2, 2, -2, -1, -2, 1])
         b = nm.array([3, -2, -4, -1])
         schur_list = nm.array([3, 4])
-        exp_x = nm.array([1, 2, 3, 4], dtype='d')
-    elif name == 'real-nonsym':
-        title = 'real-valued non-symmetric system'
+        exp_x = nm.array([1, 2, 3, 4], dtype="d")
+    elif name == "real-nonsym":
+        title = "real-valued non-symmetric system"
         ridx = nm.array([1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4])
         cidx = nm.array([1, 2, 4, 1, 2, 3, 3, 4, 1, 2, 3, 4])
         val = nm.array([2, 3, -1, 1, 2, -3, 2, -1, 2, -5, 2, 1])
         b = nm.array([4, -4, 2, 2])
         schur_list = []
-        exp_x = nm.array([1, 2, 3, 4], dtype='d')
-    elif name == 'complex-nonsym':
-        title = 'complex-valued non-symmetrix system'
+        exp_x = nm.array([1, 2, 3, 4], dtype="d")
+    elif name == "complex-nonsym":
+        title = "complex-valued non-symmetrix system"
         ridx = nm.array([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4])
         cidx = nm.array([1, 2, 3, 1, 2, 3, 2, 3, 4, 3, 4])
         val = nm.array([1 + 2j, -2j, 3 + 3j, 1j, 7, 3j, -1, 1j, 1, 3 + 5j, 2 - 3j])
         b = nm.array([-3 + 6j, 1 + 8j, -1 + 1j, 4j])
         schur_list = nm.array([3, 4])
         exp_x = nm.array([1 + 0j, 1 + 1j, 0 + 2j, 2 + 2j])
-    
+
     # build matrix
     A = sps.coo_matrix((val, (ridx - 1, cidx - 1)), shape=(4, 4))
     data = dict()
-    data['title'] = title
-    data['A'] = A
-    data['b'] = b
-    data['exp_x'] = exp_x
-    data['ridx'] = ridx
-    data['cidx'] = cidx
-    data['val'] = val
-    data['schur_list'] = schur_list
-    data['exp_x'] = exp_x
+    data["title"] = title
+    data["A"] = A
+    data["b"] = b
+    data["exp_x"] = exp_x
+    data["ridx"] = ridx
+    data["cidx"] = cidx
+    data["val"] = val
+    data["schur_list"] = schur_list
+    data["exp_x"] = exp_x
     return data
+
 
 @pytest.mark.parametrize("precision", optionsreal)
 def testRealRCDsym(precision):
     # load data
-    data = loadSystem(name='real-sym')
+    data = loadSystem(name="real-sym")
     #
     solver = mumpspy.MumpsSolver(silent=False, system=precision)
-    solver.set_rcd_mtx(data['ridx'], data['cidx'], data['val'], 4)
-    solver.set_rhs(data['b'])  # b will be overwritten!
+    solver.set_rcd_mtx(data["ridx"], data["cidx"], data["val"], 4)
+    solver.set_rhs(data["b"])  # b will be overwritten!
     x = solver.solve()  # x is b: True
     del solver
-    if precision == 'float32':
+    if precision == "float32":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
 
 @pytest.mark.parametrize("precision", optionsreal)
 def testRealMatrixsym(precision):
     # load data
-    data = loadSystem(name='real-sym')
+    data = loadSystem(name="real-sym")
     #
     solver = mumpspy.MumpsSolver(is_sym=True, silent=False, system=precision)
-    solver.set_mtx(data['A'])
-    x = solver.solve(data['b'])
+    solver.set_mtx(data["A"])
+    x = solver.solve(data["b"])
     del solver
-    if precision == 'float32':
+    if precision == "float32":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
+
+@pytest.mark.parametrize("precision", optionsreal)
+def testRealMatrixSymDirectSolve(precision):
+    # load data
+    data = loadSystem(name="real-nonsym")
+    #
+    solver = mumpspy.MumpsSolver(is_sym=False, system=precision)
+    x = solver.solve(data["A"], data["b"])
+    del solver
+    if precision == "float32":
+        _RTOL = _RTOL_single
+    else:
+        _RTOL = _RTOL_double
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
 
 @pytest.mark.parametrize("precision", optionsreal)
 def testRealMatrixNonsym(precision):
     # load data
-    data = loadSystem(name='real-nonsym')
+    data = loadSystem(name="real-nonsym")
     #
     solver = mumpspy.MumpsSolver(is_sym=False, system=precision)
-    solver.set_mtx(data['A'])
-    x = solver.solve(data['b'])
+    solver.set_mtx(data["A"])
+    x = solver.solve(data["b"])
     del solver
-    if precision == 'float32':
+    if precision == "float32":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
 
 @pytest.mark.parametrize("precision", optionsreal)
 def testRealMatrixsymMultipleRHS(precision):
     # load data
-    data = loadSystem(name='real-sym')
+    data = loadSystem(name="real-sym")
     #
     solver = mumpspy.MumpsSolver(is_sym=True, system=precision)
-    solver.set_mtx(data['A'])
-    bb = nm.vstack([2*data['b'], data['b']]).T
-    exp_xx = nm.vstack([2*data['exp_x'], data['exp_x']]).T
+    solver.set_mtx(data["A"])
+    bb = nm.vstack([2 * data["b"], data["b"]]).T
+    exp_xx = nm.vstack([2 * data["exp_x"], data["exp_x"]]).T
     xx = solver.solve(bb)
     del solver
-    if precision == 'float32':
+    if precision == "float32":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
     nm.testing.assert_allclose(xx, exp_xx, rtol=_RTOL)
 
+
 @pytest.mark.parametrize("precision", optionsreal)
 def testRealMatrixsymSchur(precision):
     # load data
-    data = loadSystem(name='real-sym')
+    data = loadSystem(name="real-sym")
     #
     solver = mumpspy.MumpsSolver(is_sym=True, system=precision)
-    solver.set_mtx(data['A'], factorize=False)
-    x = solver.schur_solve(data['schur_list'], data['b'])
+    solver.set_mtx(data["A"], factorize=False)
+    x = solver.schur_solve(data["schur_list"], data["b"])
     del solver
-    if precision == 'float32':
+    if precision == "float32":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
 
 @pytest.mark.parametrize("precision", optionsreal)
 def testRealMatrixNonsymSchur(precision):
     # load data
-    data = loadSystem(name='real-sym')
+    data = loadSystem(name="real-sym")
     #
     solver = mumpspy.MumpsSolver(is_sym=True, system=precision)
-    solver.set_mtx(data['A'], factorize=False)
-    x = solver.schur_solve(data['schur_list'], data['b'])
+    solver.set_mtx(data["A"], factorize=False)
+    x = solver.schur_solve(data["schur_list"], data["b"])
     del solver
-    if precision == 'float32':
+    if precision == "float32":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
 
 @pytest.mark.parametrize("precision", optionscomplex)
 def testComplexMatrixNonsym(precision):
     # load data
-    data = loadSystem(name='complex-nonsym')
+    data = loadSystem(name="complex-nonsym")
     #
     solver = mumpspy.MumpsSolver(system=precision)
-    solver.set_mtx(data['A'])
-    x = solver.solve(data['b'])
+    solver.set_mtx(data["A"])
+    x = solver.solve(data["b"])
     del solver
-    if precision == 'complex32':
+    if precision == "complex64":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
+
 
 @pytest.mark.parametrize("precision", optionscomplex)
 def testComplexMatrixsymSchur(precision):
     # load data
-    data = loadSystem(name='real-sym')
+    data = loadSystem(name="real-sym")
     #
     solver = mumpspy.MumpsSolver(system=precision)
-    solver.set_mtx(data['A'], factorize=False)
-    x = solver.schur_solve(data['schur_list'], data['b'])
+    solver.set_mtx(data["A"], factorize=False)
+    x = solver.schur_solve(data["schur_list"], data["b"])
     del solver
-    if precision == 'complex32':
+    if precision == "complex64":
         _RTOL = _RTOL_single
     else:
         _RTOL = _RTOL_double
-    nm.testing.assert_allclose(x, data['exp_x'], rtol=_RTOL)
-
+    nm.testing.assert_allclose(x, data["exp_x"], rtol=_RTOL)
 
 
 # title = 'real-valued symmetric system'
@@ -269,7 +291,6 @@ def testComplexMatrixsymSchur(precision):
 # del solver
 
 # print_system(title + ' (Schur complement)', A, b, x, exp_x)
-
 
 
 # solver = mumpspy.MumpsSolver(system='complex')
